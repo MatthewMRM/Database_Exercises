4591b984b9d0e70576e9f26cc72c128a5acd8063
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-06 10:12:01 -0500

    insert single statements

---
+++ albums_seeder.sql
@@ -0,0 +1,22 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genere, content)
+VALUES ('Michael Jackson', 'Thriller', '1982', '48.1', 'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genere, content)
+VALUES ('AC/DC', 'Back in Black', '1980', '25.9', 'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genere, content)
+VALUES ('Pink Floyd', 'The Dark Side of the moon', '1973', '22.7', 'Progresive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genere, content)
+VALUES ('Whitney Houston', 'The Bodyguard', '1992', '27.4', 'Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genere, content)
+VALUES ('Meat Loaf', 'Bat Out of Hell', '1977', '20.6', 'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genere, content)
+VALUES ('Eagles', 'Their Greatest Hits', '1976', '32.2', 'Rock');
+
+
+
