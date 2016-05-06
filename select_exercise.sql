d948c00d9a515c899b31a19e20693cd16236ead5
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-06 11:17:02 -0500

    inital commit

---
+++ select_exercise.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+SELECT 'The name of all albums by Pink Floyd';
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The year Thriller was released';
+SELECT * FROM albums WHERE name = 'Thriller';
+
+SELECT 'The genre for The Bodyguard';
+SELECT * FROM albums WHERE genre = 'Pop';
+
