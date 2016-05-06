013f752389595a970cfb7adce55fecd32cf0e645
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-06 11:29:01 -0500

    added selectors

--- select_exercise.sql
+++ select_exercise.sql
@@ -9,3 +9,11 @@ SELECT * FROM albums WHERE name = 'Thriller';
 SELECT 'The genre for The Bodyguard';
 SELECT * FROM albums WHERE genre = 'Pop';
 
+SELECT 'Which albums were released in the 1990s';
+SELECT * FROM albums WHERE release_date BETWEEN '1990' AND '1999';
+
+SELECT 'Whch albums had less than 20 million certified sales';
+SELECT * FROM albums WHERE sales < '20';
+
+SELECT 'All the albums in the rock genre. Is this all the rock albums in the table?';
+SELECT * FROM albums WHERE genre != 'rock';
