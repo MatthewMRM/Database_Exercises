18ac9d4370d53c52648a15684f578f6295dfa300
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-09 11:43:26 -0500

    Added group by to my query

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -4,3 +4,15 @@ FROM titles;
 SELECT DISTINCT title
 FROM titles
 GROUP BY title ASC;
+
+SELECT last_name, first_name 
+FROM employees
+WHERE (last_name LIKE 'E%'
+    AND last_name LIKE '%E')
+GROUP BY last_name, first_name ASC;
+
+SELECT last_name, first_name
+FROM employees
+WHERE last_name LIKE '%q%'
+    AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
