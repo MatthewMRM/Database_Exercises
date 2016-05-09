2a7f38793d785596eb6ea19276ac1e42dbaf2848
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-09 10:42:07 -0500

    Step 1 and Step 2 done

---
+++ order_by_exercises.sql
@@ -0,0 +1,30 @@
+SELECT first_name
+FROM employees
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya' 
+OR first_name = 'Maya';
+
+SELECT first_name
+FROM employees
+WHERE gender = 'M'
+	AND (first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya');
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+	OR last_name LIKE '%E';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+	AND hire_date LIKE '199%-%-%';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%';
+	AND last_name NOT LIKE '%qu%';
+
+
+
